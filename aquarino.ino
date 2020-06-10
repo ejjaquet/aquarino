ae9aa852961e55ede8b5f7df5282d303455561d8
ejjaquet <erikjan@jaquet.nl>
2020-06-10 10:30:33 +0200

    Added wifiman and switched to Blynk

--- aquarino.ino
+++ aquarino.ino
@@ -5,19 +5,18 @@
  *
  * PIN AANSLUITINGEN PH SENSOR
  *
- * Vcc  --> 5V  (rood)
- * Data --> A0  (groen)
- * GND  --> GND (zwart)
- * GND --> GND (zwart)
+ * Vcc  --> 5V
+ * Data --> A0
+ * GND  --> GND
+ * GND --> GND
  *
  *
  * PIN AANSLUITINGEN 1-wire SENSOR
  *
- * GND  --> GND (zwart)
- * Data --> D6  (geel)
- * Vcc  --> 5V  (rood)
+ * GND  --> GND
+ * Data --> D7
+ * Vcc  --> 5V
  *
- * 4K7 (4700) Ohm weerstand tussen 5V en Data pin
  *
  *
  * PIN AANSLUITINGEN LCD I2C
@@ -25,14 +24,63 @@
  * SDA is serial data
  * SCL is serial clock
  *
- * GND --> GND (wit)
- * VCC --> 5V  (grijs)
- * SDA --> D2  (rood)
- * SCL --> D1  (blauw)
+ * GND --> GND
+ * VCC --> 5V
+ * SDA --> D2
+ * SCL --> D1
  * 
  *  
  */
 
+//// Blynk (iot) ////
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <BlynkSimpleEsp8266.h>
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include "WiFiManager.h"          //https://github.com/tzapu/WiFiManager
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "thEHa5ITbUeO5xq7ITLAVyp4Ek3lLzcX";
+
+BlynkTimer timer;
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, millis() / 1000);
+}
+
+//// PH-SENSOR ////
+
+static const int analogInPin = A0; 
+int sensorValue = 0; 
+unsigned long int avgValue;
+float calibration = -0.50;               //change this value to calibrate
+float b;
+int buf[10],temp;
+
+float phValue = 0.0;                    // variabele om ph waarde in op te slaan (float is getal met komma)
+
+int intervalPh = 1000;                  // Tijd in milliseconden tussen het uitlezen van de ph sensor
+unsigned long previousMillisPh = 0;     // Tijdstip van laatste uitlezing ph sensor
 
 //// LCD SCHERM ////
 
@@ -113,72 +161,30 @@ byte minIcon[] = {
 
 bool redrawLCD = true;                  // geeft aan of het scherm moet worden bijgewerkt
 
-
-//// PH-SENSOR ////
-
-static const int analogInPin = A0; 
-int sensorValue = 0; 
-unsigned long int avgValue; 
-float b;
-int buf[10],temp;
-
-float phValue = 0.0;           // variabele om ph waarde in op te slaan (float is getal met komma)
-
-int intervalPh = 1000;               // Tijd in milliseconden tussen het uitlezen van de ph sensor
-unsigned long previousMillisPh = 0;           // Tijdstip van laatste uitlezing ph sensor
-
 //// 1-WIRE DS18B20 SENSOR ////
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-static const int oneWireBus = D6;        // 1-wire pin
-OneWire oneWire(oneWireBus);            // 1-wire instantie op de oneWireBus pin
-DallasTemperature sensors(&oneWire);    // geef de instantie van de oneWire bus door aan de DallasTemperature
+static const int oneWireBus = D5;         // 1-wire pin
+OneWire oneWire(oneWireBus);              // 1-wire instantie op de oneWireBus pin
+DallasTemperature sensors(&oneWire);      // geef de instantie van de oneWire bus door aan de DallasTemperature
 
-float temperatureValue = 0.0;           // variabele om temperatuur in op te slaan (float is getal met komma)
-float temperatureMin = 0.0;           // variabele om de laagst gemeten temperatuur in op te slaan
-float temperatureMax = 0.0;             // variabele om de hoogst gemeten temperatuur in op te slaan
+float temperatureValue = 0.0;             // variabele om temperatuur in op te slaan (float is getal met komma)
+float temperatureMin = 0.0;               // variabele om de laagst gemeten temperatuur in op te slaan
+float temperatureMax = 0.0;               // variabele om de hoogst gemeten temperatuur in op te slaan
 
-int interval1Wire = 1000;               // Tijd in milliseconden tussen het uitlezen van de 1-wire sensor
-unsigned long previousMillis1Wire = 0;           // Tijdstip van laatste uitlezing 1-wire sensor
+int interval1Wire = 1000;                 // Tijd in milliseconden tussen het uitlezen van de 1-wire sensor
+unsigned long previousMillis1Wire = 0;    // Tijdstip van laatste uitlezing 1-wire sensor
 
-//// WIFI (onboard ESP 8266) ////
-#include "AdafruitIO_WiFi.h"
- 
-#define WIFI_SSID       "TELE2-2FCOE3_2.4G"
-#define WIFI_PASS       "2744C3775C5E"
- 
-#define IO_USERNAME    "ejjaquet"
-#define IO_KEY         "4347a23da4b7433c9b42f519b2228ab2"
- 
-// Connect to Wi-Fi and Adafruit IO handel 
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
- 
-// Create a feed object that allows us to send data to
-AdafruitIO_Feed *phFeed = io.feed("ph");
-AdafruitIO_Feed *temperatureFeed = io.feed("temperature");
-AdafruitIO_Feed *tempMinFeed = io.feed("tempmin");
-AdafruitIO_Feed *tempMaxFeed = io.feed("tempmax");
 
 //// ALGEMEEN ////
 
-unsigned long currentMillis = 0;        // Variabele voor het aantal milliseconden sinds de Arduino is gestart
-
-//// FUNCTIES ////
+unsigned long currentMillis = 0;           // Variabele voor het aantal milliseconden sinds de Arduino is gestart
 
 void setup() {
-  Serial.begin(115200);                 // stel de seriële monitor in
-  delay(500);
-
-// Connect to Adafruit IO
-  io.connect();
- 
-  // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
-    Serial.println('.');
-    delay(500);
-  }
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
   // LCD
   lcd.init();                           // initialiseer het LCD scherm
@@ -189,9 +195,42 @@ void setup() {
   lcd.createChar(3, maxIcon);           // definieer een symbool in geheugen positie 3
   lcd.createChar(4, minIcon);           // definieer een symbool in geheugen positie 4
   lcd.clear();                          // start met een leeg scherm
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if(!wifiManager.autoConnect("Aquarino")) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+
+  } 
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+  Blynk.config(auth);
+
+  if(!Blynk.connected()) {
+    Blynk.connect();
+  }
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
 
   // Temperature sensor
-  sensors.begin();                      // start het uitlezen van de temperatuur sensor
+  sensors.begin();                        // start het uitlezen van de temperatuur sensor
 
 }
 
@@ -206,26 +245,27 @@ void read1WireTemp() {
 
     // neem de temperatuur van de eerste 1-wire sensor
     float readTemperatureValue = sensors.getTempCByIndex(0);
+    Serial.println(readTemperatureValue);
 
     // Controleer of de gelezen waarde anders is dan de vorige waarde
     if(temperatureValue != readTemperatureValue) {
       temperatureValue = readTemperatureValue;
       redrawLCD = true;
-      temperatureFeed->save(temperatureValue);
+      Blynk.virtualWrite(V5, temperatureValue);
     }
 
     // Controleer of de gelezen waarde groter is dan de opgeslagen max. temperatuur
     if (temperatureValue > temperatureMax) {
         temperatureMax = temperatureValue;
         redrawLCD = true;
-        tempMaxFeed->save(temperatureMax);
+        Blynk.virtualWrite(V6, temperatureMax);
     }
 
     // Controleer of de gelezen waarde groter is dan de opgeslagen min. temperatuur
     if (temperatureValue < temperatureMin || temperatureMin == 0.00) {
         temperatureMin = temperatureValue;
         redrawLCD = true;
-        tempMinFeed->save(temperatureMin);
+        Blynk.virtualWrite(V7, temperatureMin);
     }
 
     // sla het huidige tijdstip op in previousMillis1Wire
@@ -235,7 +275,6 @@ void read1WireTemp() {
 
 }
 
-// Functie voor het uitlezen van de DHT sensor
 void readPhValue() {
 
   // Als het tijdsverschil met de vorige uitlezing groter is dan intervalDHT
@@ -244,7 +283,7 @@ void readPhValue() {
     // vraag de PH waarde aan de PH sensor
     for(int i=0;i<10;i++) { 
       buf[i]=analogRead(analogInPin);
-      delay(10);
+      delay(30);
     }
 
     for(int i=0;i<9;i++) {
@@ -261,8 +300,9 @@ void readPhValue() {
  
     for(int i=2;i<8;i++)
     avgValue+=buf[i];
-    float pHVol=(float)avgValue*5.0/1024/6;
-    float myPhValue = pHVol + 2.34;
+    Serial.println(avgValue);
+    float pHVol=(float)avgValue*3.3/1023/6;
+    float myPhValue = 3.3 * pHVol + calibration;
 
     // Controleer of de gelezen waarde een getal is (Nan = Not A Number)
     if (isnan(myPhValue)) {
@@ -274,7 +314,7 @@ void readPhValue() {
     if (myPhValue != phValue) {
       phValue = myPhValue;     // werk PhValue bij met de nieuwe waarde
       redrawLCD = true;        // geef aan dat het LCD bijgewerkt moet worden
-      phFeed->save(phValue);
+      Blynk.virtualWrite(V4, phValue);
     }
 
     previousMillisPh = currentMillis;   // sla het huidige tijdstip op in previousMillisDHT
@@ -290,7 +330,7 @@ void updateLCD() {
   if(redrawLCD) {
     
     lcd.clear();                 // wis het scherm
-  
+      
     lcd.setCursor(0, 0);         // zet de cursor op positie 1, regel 1
     lcd.write(0);                // teken het ph waarde icoon van geheugenpositie 0
     lcd.print(" ");              // schrijf een spatie
@@ -324,12 +364,14 @@ void updateLCD() {
 
 }
 
-
 void loop() {
-  io.run();
-  currentMillis = millis();    // sla de huidige tijd op
-  readPhValue();               // lees de PH sensor uit
-  read1WireTemp();             // lees de 1-wire temperatuur sensor uit
-  updateLCD();                 // werk indien nodig het LCD scherm bij
-  delay(5000);
+      // put your main code here, to run repeatedly:
+      Blynk.run();
+      // timer.run(); // Initiates BlynkTimer
+
+      currentMillis = millis();    // sla de huidige tijd op
+      read1WireTemp();             // lees de 1-wire temperatuur sensor uit
+      readPhValue();               // lees de Ph sensor uit
+      updateLCD();                 // update lcd scherm met actuele waarden
+      delay(5000);
 }
